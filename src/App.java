014023653a81a30094b28742c71f2fa0204be925
RickBm400 <rjbm29@gmail.com>
2024-10-02 19:06:32 -0500

    feat: Data user class

--- src/App.java
+++ src/App.java
@@ -3,18 +3,32 @@ import java.io.Console;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("que pasa puta");
-        User Ricko = User.createUser("Rikoso");
-        System.out.println(Ricko.getName());
-        System.out.println("Hola de nuevo " + Ricko.getName());
+        // System.out.println("que pasa puta");
+        // User Ricko = User.createUser("Rikoso");
+        // System.out.println(Ricko.getName());
+        // System.out.println("Hola de nuevo " + Ricko.getName());
 
-        Console con = System.console();
-        if (con == null) {
-            System.err.println("No console");
-            System.exit(1);
+        // Console con = System.console();
+        // if (con == null) {
+        // System.err.println("No console");
+        // System.exit(1);
+        // }
+
+        Console input = System.console();
+        Integer totalPeople = Integer.parseInt(input.readLine("Numero de personas en el poblado"));
+
+        DataUser listaPoblado = DataUser.setPoblado(totalPeople);
+
+        for (int i = 0; i < listaPoblado.getLength(); i++) {
+            String name = input.readLine("Nombre para el usuario " + i + "\n");
+            Integer age = Integer.parseInt(input.readLine("Edad del usuario: "));
+
+            User pruebas = User.createUser(name, age);
+            System.out.println("Usuario creado");
+
+            listaPoblado.AddToPoblado(i, pruebas);
         }
 
-        String log = con.readLine("\n Enter logoso:");
-        System.out.println(log + "> thafk logoso ");
+        listaPoblado.getPoblado();
     }
 }
--- src/classess/User.java
+++ src/classess/User.java
@@ -1,17 +1,28 @@
 package classess;
+
+import java.util.Optional;
+
 public class User {
     public String name;
-    public int age = 0;
+    public int age;
 
-    private User(String name) {
+    private User(String name, Optional<Integer> age) {
+        this.age = age.orElse(0);
         this.name = name;
     }
-    
-    public static User createUser(String name) { // Factory pattern
-        return new User(name);
+
+    public static User createUser(String name, Integer age) { // Factory pattern
+        return new User(name, Optional.of(age));
     }
 
     public String getName() {
         return this.name;
     }
+
+    static void classOla() {
+        System.out.println("olasas");
+    }
+
+
+
 }
